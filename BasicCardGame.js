6cef19ea6d83b1a423aaeb5fc01cd9be7528cbbd
julianbei <julian@contentpass.de>
2017-12-04 19:28:18 +0100

    Refactoring code style

--- BasicCardGame.js
+++ BasicCardGame.js
@@ -1,81 +1,116 @@
-var deck = [];                          //Nachziehstapel
-var stack = [];                         //Ablagestapel
-var graveyard = [];                     //Friedhof für bereits ausgewertete Karten
-var enemyHealthPoints = [];             //NPC Lebenspunkte
-var playerHealthPoints = [];            //Spieler Lebenspunkte
-var numberOfHandCards;                  //Anzahl der maximalen Handkarten
-var enemyHandCards = [];                //Handkarten des NPC's
-var playerHandCards = [];               //Handkarten des Spielers
+var deck = []; //Nachziehstapel
+var stack = []; //Ablagestapel
+var graveyard = []; //Friedhof für bereits ausgewertete Karten
+var enemyHealthPoints = []; //NPC Lebenspunkte
+var playerHealthPoints = []; //Spieler Lebenspunkte
+var numberOfHandCards; //Anzahl der maximalen Handkarten
+var enemyHandCards = []; //Handkarten des NPC's
+var playerHandCards = []; //Handkarten des Spielers
 
 
-window.onload = preGameFunction();      //automatischer Scriptstart bei aufrufen der Seite
+window.onload = preGameFunction(); //automatischer Scriptstart bei aufrufen der Seite
 
 
-function preGameFunction() {            //vorbereitende Funktion
+function preGameFunction() { //vorbereitende Funktion
 
-    enemyHealthPoints, playerHealthPoints = [20,20];        //Festsetzen der Spielerlebenspunkte
-    numberOfHandCards = 5;                                  //Festsetzen der maximale Handkartenzahl
-    deck = ["Funken","Funken","Funken","Funken","Funken","Glut","Glut","Glut","Glut","Feuerbal","Feuerbal","Feuerbal","Sturm","Sturm","Sturm","Sturm","Sturm","Orkan","Orkan","Orkan","Panic Button","Panic Button","Panic Button","Panic Button","Blithschlag","Blithschlag","Blithschlag","Blithschlag","Erdbeben","Erdbeben","Erdbeben","Heilender Segen","Heilender Segen","Heilender Segen","Heilender Segen","Sintflut","Sintflut","Eiszeit","Eiszeit","Eiszeit"];
-   //Deck deklarieren
-    evaluationRound();                                     //Übergang in die Auswertungsrunde um Starthände zu verteilen
+  enemyHealthPoints,
+  playerHealthPoints = [20, 20]; //Festsetzen der Spielerlebenspunkte
+  numberOfHandCards = 5; //Festsetzen der maximale Handkartenzahl
+  deck = [
+    "Funken",
+    "Funken",
+    "Funken",
+    "Funken",
+    "Funken",
+    "Glut",
+    "Glut",
+    "Glut",
+    "Glut",
+    "Feuerbal",
+    "Feuerbal",
+    "Feuerbal",
+    "Sturm",
+    "Sturm",
+    "Sturm",
+    "Sturm",
+    "Sturm",
+    "Orkan",
+    "Orkan",
+    "Orkan",
+    "Panic Button",
+    "Panic Button",
+    "Panic Button",
+    "Panic Button",
+    "Blithschlag",
+    "Blithschlag",
+    "Blithschlag",
+    "Blithschlag",
+    "Erdbeben",
+    "Erdbeben",
+    "Erdbeben",
+    "Heilender Segen",
+    "Heilender Segen",
+    "Heilender Segen",
+    "Heilender Segen",
+    "Sintflut",
+    "Sintflut",
+    "Eiszeit",
+    "Eiszeit",
+    "Eiszeit"
+  ];
+  //Deck deklarieren
+  evaluationRound(); //Übergang in die Auswertungsrunde um Starthände zu verteilen
 
 }
 
 function evaluationRound() {
 
-    while (stack.length >= 1){
-        cardEffects(stack[stack.length - 1],stack.length -2);
-        graveyard.push(stack[stack.length-2])
-        stack.length = stack.length - 2;
+  while (stack.length >= 1) {
+    cardEffects(stack[stack.length - 1], stack.length - 2);
+    graveyard.push(stack[stack.length - 2])
+    stack.length = stack.length - 2;
 
-    }
-    //if (stack.length = 0; playerHandCards.length < numberOfHandCards || enemyHandCards.length < numberOfHandCards) {
+  }
+  //if (stack.length = 0; playerHandCards.length < numberOfHandCards || enemyHandCards.length < numberOfHandCards) {
 
-        swapHealth();
-        drawCards(playerHandCards);
-        drawCards(enemyHandCards);
-        stackingRound();
-        }
+  swapHealth();
+  drawCards(playerHandCards);
+  drawCards(enemyHandCards);
+  stackingRound();
+}
 
 function stackingRound() {
 
-    if (playerHandCards > 0 || enemyHandCards > 0) {
+  if (playerHandCards > 0 || enemyHandCards > 0) {
 
 
-    }
+  }
 }
 
-function swapHealth(){
-        var storeValue,storeIndex;
-        storeValue = Math.min(...playerHealthPoints);
-        storeIndex = enemyHealthPoints.indexOf(Math.min(...enemyHealthPoints));
-        playerHealthPoints[playerHealthPoints.indexOf(Math.min(...playerHealthPoints))] = Math.min(...enemyHealthPoints);
-        enemyHealthPoints[storeIndex] = storeValue;
+function swapHealth() {
+  var storeValue, storeIndex;
+  storeValue = Math.min(...playerHealthPoints);
+  storeIndex = enemyHealthPoints.indexOf(Math.min(...enemyHealthPoints));
+  playerHealthPoints[playerHealthPoints.indexOf(Math.min(...playerHealthPoints))] = Math.min(...enemyHealthPoints);
+  enemyHealthPoints[storeIndex] = storeValue;
+}
+
+function drawCard(targetHand) {
+  var i;
+
+  while (targetHand.length < numberOfHandCards) {
+    if (deck.length > 0) {
+      i = Math.floor(Math.random() * deck.length);
+
+      if (i in deck) {
+        targetHand.push(deck[i]);
+        deck.splice(i, 1);
+      }
+
+    } else if (deck.length < 1) {
+      deck = graveyard;
+      graveyard.length = 0;
     }
 
-function drawCard(targetHand){
-
-        var i;
-
-        while (targetHand.length < numberOfHandCards) {
-        if (deck.length > 0) {
-            i = Math.floor(Math.random() * deck.length)
-
-            if (i in deck) {
-
-                targetHand.push(deck[i]);
-                deck.splice(i,1);
-
-            }
-
-        }
-
-        else if (deck.length < 1){
-            deck = graveyard;
-            graveyard.length = 0;
-            }
-
-        }
-
-    }
-
+  }
+}
